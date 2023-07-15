4282179169d105e1a132b90a4b216d60385b41ff
Carmina Familar <Carmina@MacBook-Pro-4.local>
2023-07-15 13:34:05 +0930

    Initial setup

---
+++ init.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+'''
+Instances of the libraries/toolkit/packages are being initialized below so that
+you can just import them to main using its variable names
+
+- SQL Alchemy: Python SQL toolkit and Object Relational Mapper
+- Marshmallow: Object Relational Mapping library to convert objects (such as database schemas) to and from Python data types
+- Bcrypt: Library for generating strong hashing values
+- JWTManager: For JSON Web Token (JWT) Authentication
+'''
+db = SQLAlchemy()
+ma = Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager
